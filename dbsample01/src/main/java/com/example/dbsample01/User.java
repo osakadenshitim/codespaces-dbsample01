6706dba426342266abee652c1fb934a0c6b6d4dc
cr1teacher <ikeda_toshiaki@edu.kimura.ac.jp>
2025-05-07 12:19:39 +0900

    cr1test

--- dbsample01/src/main/java/com/example/dbsample01/User.java
+++ dbsample01/src/main/java/com/example/dbsample01/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//test coment
 @Entity
 @Table(name = "\"user\"")
 public class User {
